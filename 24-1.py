58871709bab28c48a89d94384d78614162f4371e
Maxime Goyette <maxime.goyette.2@ens.etsmtl.ca>
2017-12-24 01:46:26 -0500

    Day 24

---
+++ 24-1.py
@@ -0,0 +1,32 @@
+a = open('24.txt').read()
+
+c = [tuple(map(int , x.split('/'))) for x in  a.split('\n')]
+
+b = {}
+
+
+while True:
+    s = [(0, 0)]
+
+    a = c[:]
+
+    while True:
+        n = len(a)
+        for x in a:
+            if s[-1][1] in x:
+                if b.get(tuple(s + [x]), None) == None and x[0] == s[-1][1]:
+                    s.append(x)
+                    a.remove(x)
+                elif b.get(tuple(s + [x[::-1]]), None) == None and x[1] == s[-1][1]:
+                    s.append(x[::-1])
+                    a.remove(x)
+                
+        if len(a) == n:
+            break
+
+    if s == [(0, 0)]:
+        break
+    else:
+        b[tuple(s)] = sum(map(sum, s))
+
+print max([b[x] for x in b])
